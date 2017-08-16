313a87991c53670bfa92a2235995c3e575f139a4
xtophs <christoph@bracketangles.net>
2017-08-17 01:26:01 +0200

    Building Cassandra Cluster nodes using Azure Metadata Service and Google's Cassandra Cluster node base image

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM cassandra:latest
+FROM gcr.io/google-samples/cassandra:v12
 MAINTAINER xtophs
-LABEL Name=azcassandra Version=0.0.1
+LABEL Name=azcassandra Version=0.0.2
 RUN apt update && apt install curl -y
 COPY startcassandra.sh /usr/local/bin/startcassandra.sh
-ENTRYPOINT /bin/bash '/usr/local/bin/startcassandra.sh'
+CMD [ "/sbin/dumb-init", "/bin/bash",  "/usr/local/bin/startcassandra.sh"]
--- startcassandra.sh
+++ startcassandra.sh
@@ -2,24 +2,24 @@
 set -e
 
 echo checking metadata
-fd=$( "http://169.254.169.254/metadata/instance/compute/platformFaultDomain?api-version=2017-04-02&format=text" )
+fd=$( curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/platformFaultDomain?api-version=2017-04-02&format=text" )
 echo got fault domain $fd
-
+location=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/location?api-version=2017-07-01&format=text")
+echo got location $location
 if [ -z $fd ]
 then
-  echo No FD
+  echo No Azure Fault Domain
   exit 1
 fi
 
 echo Deleteing properties file
 rm -f /etc/cassandra/cassandra-rackdc.properties
 
-echo dc=dc1 > /etc/cassandra/cassandra-rackdc.properties
-echo rack=rack$fd >> /etc/cassandra/cassandra-rackdc.properties
-if [ ! -z $CASSANDRA_RACK ]
-then
-     CASSANDRA_RACK=rack$fd
-     export CASSANDRA_RACK
-     echo set env $CASSANDRA_RACK
-fi
-. /docker-entrypoint.sh cassandra -f
+echo dc=dc-$location > /etc/cassandra/cassandra-rackdc.properties
+echo rack=rack-$fd >> /etc/cassandra/cassandra-rackdc.properties
+export CASSANDRA_RACK=rack-$fd
+echo set env CASSANDRA_RACK=$CASSANDRA_RACK
+export CASSANDRA_DC=dc-$location
+echo set env CASSANDRA_DC=dc-$location
+
+. /run.sh
